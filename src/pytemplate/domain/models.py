47bd0375027447eaea638edc88904c6d88c84546
Rashad Musayev <rmusayevr19@gmail.com>
2024-08-01 17:13:32 +0400

    fix: type error

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import marshmallow
 
-from src.pytemplate.domain.validators import BurgerSchema
+from pytemplate.domain.validators import BurgerSchema
 
 
 @dataclass
